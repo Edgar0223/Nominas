5031b36bead486dcc1e8b4de281d965c08d1ad22
Edgar0223 <142974179+Edgar0223@users.noreply.github.com>
2024-10-21 23:28:02 -0600

    conexion a la base de datos

--- Nominas/settings.py
+++ Nominas/settings.py
@@ -97,13 +97,7 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': '35.239.235.125',                  
         'PORT': '3306',    
-        'OPTIONS': {
-            'ssl': {
-                'ca': '../ssl/server-ca.pem',  # Deja los campos SSL vacíos para deshabilitar la verificación
-                'cert': './ssl/client-cert.pem',
-                'key': './ssl/client-key.pem',
-            },
-        },               
+        'OPTIONS': {},               
     }
 }
 
