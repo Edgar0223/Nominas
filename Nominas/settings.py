6fa6dfca8f977f7ff32c6c000af676248459c7e2
Adrian Jimenez <111606136+AdrianJimenez7u7@users.noreply.github.com>
2024-10-21 23:03:13 -0600

    credenciales

--- Nominas/settings.py
+++ Nominas/settings.py
@@ -91,8 +91,19 @@ WSGI_APPLICATION = 'Nominas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Nominas', 
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '35.239.235.125',                  
+        'PORT': '3306',    
+        'OPTIONS': {
+            'ssl': {
+                'ca': '../ssl/server-ca.pem',  # Deja los campos SSL vacíos para deshabilitar la verificación
+                'cert': './ssl/client-cert.pem',
+                'key': './ssl/client-key.pem',
+            },
+        },               
     }
 }
 
