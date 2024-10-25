5e161dbed09491ad21b7525cb5ddbfccaf95b241
Edgar0223 <142974179+Edgar0223@users.noreply.github.com>
2024-10-24 21:32:59 -0600

    cambio de nombre de relacion

--- apps/Nomina/models.py
+++ apps/Nomina/models.py
@@ -11,7 +11,7 @@ class Nominas(models.Model):
     pagoNomina = models.DecimalField(max_digits=10, decimal_places=3, validators=[MinValueValidator(0)])
     status = models.BooleanField(default=False)
     diasPagadas = models.IntegerField()
-    empelado = models.ForeignKey('Empleados.Empleados', on_delete=models.CASCADE, related_name='empleados_en_departamento', null=True)
+    empelado = models.ForeignKey('Empleados.Empleados', on_delete=models.CASCADE, related_name='empleados_en_nominas', null=True)
     def __str__(self):
         return f'{self.fechaInicio} {self.fechaFin} {self.pagoNomina}'
 
