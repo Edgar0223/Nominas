24ad537741a7d149666bb48665c8c0290a0168e9
Edgar0223 <142974179+Edgar0223@users.noreply.github.com>
2024-10-24 21:28:58 -0600

    creacion tabla nomina

--- apps/Nomina/models.py
+++ apps/Nomina/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Nominas(models.Model):
+    fechaInicio = models.DateField(auto_now_add=True)
+    fechaFin = models.DateField(auto_now_add=True)
+    pagoNomina = models.DecimalField(max_digits=10, decimal_places=3, validators=[MinValueValidator(0)])
+    status = models.BooleanField(default=False)
+    diasPagadas = models.IntegerField()
+    empelado = models.ForeignKey('Empleados.Empleados', on_delete=models.CASCADE, related_name='empleados_en_departamento', null=True)
+    def __str__(self):
+        return f'{self.fechaInicio} {self.fechaFin} {self.pagoNomina}'
+
+
