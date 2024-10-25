60cd4d06708c79e63b7242d2fc9fe9ec98c66d82
Edgar0223 <142974179+Edgar0223@users.noreply.github.com>
2024-10-24 21:56:46 -0600

    creacion de materiaprima y mas

--- apps/Empresa/models.py
+++ apps/Empresa/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
 # Create your models here.
 
@@ -14,3 +15,22 @@ class datosEmpresa(models.Model):
     valores = models.CharField(max_length=254)
     mision = models.CharField(max_length=254)
     logo  = models.ImageField(upload_to='logo/', null=True, blank=True)
+
+class Personas(models.Model):
+    Nombre = models.CharField(max_length=254)
+    rfc = models.CharField(max_length=254)
+    razonSocial = models.CharField(max_length=254)
+    direccion = models.CharField(max_length=254)
+    pagina = models.CharField(max_length=254)
+    telefono = models.CharField(max_length=13)
+    caracterPersona = models.CharField(max_length=254)
+    materiaP = models.ForeignKey('MateriaPrima.materiaPrima', on_delete=models.CASCADE, related_name='clientes_o_provedores_en_contacto', null=True)
+
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=254)
+    telefono = models.CharField(max_length=13)
+    correo = models.EmailField(max_length=100, unique=True, validators=[EmailValidator(message="Introduce un correo válido.")])
+    tipo = models.CharField(max_length=254)
+    persona = models.ForeignKey(Personas, on_delete=models.CASCADE, related_name='clientes_o_provedores_en_contacto', null=True)
+
--- apps/MateriaPrima/models.py
+++ apps/MateriaPrima/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Area(models.Model):
+    nombre =  models.CharField(max_length=255)
+    unbicacion = models.CharField(max_length=255)
+
+class materiaPrima(models.Model):
+    nombre = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=255)
+    incioZafra = models.DateField(auto_now_add=True)
+    finZafra = models.DateField(auto_now_add=True)
+
+class Procesos(models.Model):
+    nombre = models.CharField(max_length=255)
+    fecha =  models.DateField(auto_now_add=True)
+    areas  = models.ForeignKey(Area, on_delete=models.CASCADE, related_name='areas_en_procesos', null=True)
+    materia  = models.ForeignKey(materiaPrima, on_delete=models.CASCADE, related_name='materia_utizada_en_procesos', null=True)
